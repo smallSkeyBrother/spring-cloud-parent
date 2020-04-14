cbd8982ab5fef5091ee9ce278876aef9c6904bfa
王飞龙 <wangfeilong@lixin360.com>
2020-04-14 15:58:17 +0800

    second time init

--- spring-cloud-producer/src/main/java/com/spring/producer/ProducerApplication.java
+++ spring-cloud-producer/src/main/java/com/spring/producer/ProducerApplication.java
@@ -2,6 +2,7 @@ package com.spring.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  **/
 @SpringCloudApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class ProducerApplication {
 
     public static void main(String[] args) {
