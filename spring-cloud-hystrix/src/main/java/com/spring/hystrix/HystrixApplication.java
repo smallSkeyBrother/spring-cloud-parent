08fd823d20a9be5917431cb68fdfb7e34c28ae2f
王飞龙 <wangfeilong@lixin360.com>
2020-04-21 16:35:59 +0800

    second time init

--- spring-cloud-hystrix/src/main/java/com/spring/hystrix/HystrixApplication.java
+++ spring-cloud-hystrix/src/main/java/com/spring/hystrix/HystrixApplication.java
@@ -1,22 +1,22 @@
-package com.spring.producer;
+package com.spring.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author wangfeilong
  * @version v1.0
  * @Description //TODO
- * @Date 2020/4/13 16:27
+ * @Date 2020/4/21 10:41
  **/
 @SpringCloudApplication
-@EnableEurekaClient
-//@EnableDiscoveryClient
-public class ProducerApplication {
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+public class HystrixApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProducerApplication.class, args);
+        SpringApplication.run(HystrixApplication.class, args);
     }
 }
