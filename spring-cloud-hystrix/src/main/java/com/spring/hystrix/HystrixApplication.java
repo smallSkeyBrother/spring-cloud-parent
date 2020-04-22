9fc1b27ab1e20cd1084fb2931eb9c2460e9d2a11
王飞龙 <wangfeilong@lixin360.com>
2020-04-22 14:41:48 +0800

    second time init

--- spring-cloud-hystrix/src/main/java/com/spring/hystrix/HystrixApplication.java
+++ spring-cloud-hystrix/src/main/java/com/spring/hystrix/HystrixApplication.java
@@ -2,18 +2,17 @@ package com.spring.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author wangfeilong
  * @version v1.0
  * @Description //TODO
  * @Date 2020/4/21 10:41
+ *
+ * @EanbleHystrix 等同于 @EnableCircuitBreaker
+ * 
  **/
 @SpringCloudApplication
-@EnableDiscoveryClient
-@EnableCircuitBreaker
 public class HystrixApplication {
 
     public static void main(String[] args) {
--- spring-cloud-producer/src/main/java/com/spring/producer/ProducerApplication.java
+++ spring-cloud-producer/src/main/java/com/spring/producer/ProducerApplication.java
@@ -2,21 +2,26 @@ package com.spring.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author wangfeilong
  * @version v1.0
  * @Description //TODO
  * @Date 2020/4/13 16:27
+ *
+ * @SpringCloudApplication 注解里面包含了一下注解
+ *
+ *   @SpringBootApplication    SpringBoot项目启动服务
+ *   @EnableDiscoveryClient   引进服务发现
+ *   @EnableCircuitBreaker    引进断路器
+ *
  **/
 @SpringCloudApplication
-@EnableEurekaClient
-//@EnableDiscoveryClient
+
 public class ProducerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProducerApplication.class, args);
     }
+
 }
